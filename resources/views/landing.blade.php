f897009f2e3038bb278ea7083feaa6d49e1587f4
Ramon Saldanha <ramonsaldanhaa@gmail.com>
2020-05-20 22:33:42 -0300

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('public/css/main.css')}}">
+  <title>Hello, world!</title>
+</head>
+<body>
+  <div class="main-section">
+    <div class="container">
+      <h2>
+        <strong>Nunca mais perca tempo</strong> com atividades repetitivas,<br />
+        otimize seu tempo automatizando seu processo de criação de petições
+      </h2>
+      <iframe class="my-4" width="560" height="315" src="https://www.youtube.com/embed/etjd0X-6n8Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p>
+        Com esse magnífico software, você advogado poderá automatizar a criação daquelas peças em você costuma perder mais tempo. Sabemos que tempo é dinheiro, você pode usa-lo para aquelas atividades que demandam mais raciocínio.
+      </p>
+    </div>
+  </div>
+  
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
